003b6aa4fa82e750a0d2d976f24a3126b7ce0478
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2015-11-26 18:59:31 +0100

    Ajout mesure dfauts de cache avec lib papi et factorisation du Makefile avec un mode de debug

--- jeremie/src/computePatchOpt.cpp
+++ jeremie/src/computePatchOpt.cpp
@@ -4,6 +4,11 @@
 #include <ctime>
 #include <stdexcept>
 
+#ifdef PAPI
+#include <stdio.h>
+#include <papi.h>
+#endif
+
 using namespace std;
 
 
@@ -17,6 +22,19 @@ int main (int argc, char **argv) {
         }
 
         const time_t START = time(NULL);
+
+        #ifdef PAPI
+        long long counters[3];
+        int PAPI_events[] = {
+            PAPI_TOT_CYC,
+            PAPI_L2_DCM,
+            PAPI_L2_DCA };
+
+        PAPI_library_init(PAPI_VER_CURRENT);
+        const int i = PAPI_start_counters( PAPI_events, 3 );
+        (void)i;
+        #endif
+
         Solver solver(argv[1], argv[2]);
 
         cerr << "Calcul des couts..." << endl;
@@ -35,6 +53,17 @@ int main (int argc, char **argv) {
         cerr << "Ecriture terminee !" << endl << endl;
         cerr << "Duree totale : " << time(NULL) - START
         	 << " secondes !" << endl << endl;
+
+
+        #ifdef PAPI
+        PAPI_read_counters( counters, 3 );
+
+        printf("%lld L2 cache misses (%.3lf%% misses) in %lld cycles\n", 
+            counters[1],
+            (double)counters[1] / (double)counters[2],
+            counters[0] );
+        #endif
+
 
         return EXIT_SUCCESS;
     }
